22c9d7e6d94f8b9339157c95f4ef3311554c36db
mcvagut34 <moises.valda@ucb.edu.bo>
2023-12-21 05:38:09 -0400

    ddd

--- script.bat
+++ script.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 # Archivo específico a excluir durante el merge (con ruta relativa)
@@ -10,32 +9,12 @@ git stash
 # Realizar el merge
 git merge dev
 
-# Deshacer los cambios específicos en la rama de destino (main)
-git checkout main -- "$EXCLUDED_FILE"
+# Deshacer los cambios específicos en la rama de destino (main) si hay conflictos
+git checkout --ours -- "$EXCLUDED_FILE"
 
 # Commit de los cambios en la rama de destino (main)
+git add "$EXCLUDED_FILE"
 git commit -m "Merge de dev en main, excluyendo cambios en $EXCLUDED_FILE"
 
 # Aplicar los cambios locales nuevamente (sin aplicar los cambios del archivo específico)
 git stash apply --index
-=======
-@echo off
-
-REM Archivo específico a excluir durante el merge (con ruta relativa)
-set "EXCLUDED_FILE=src\router\index.js"
-
-REM Realizar un stash de los cambios locales en la rama de destino (main)
-git stash
-
-REM Realizar el merge
-git merge dev
-
-REM Deshacer los cambios específicos en la rama de destino (main)
-git checkout main -- "%EXCLUDED_FILE%"
-
-REM Commit de los cambios en la rama de destino (main)
-git commit -m "Merge de dev en main, excluyendo cambios en %EXCLUDED_FILE%"
-
-REM Aplicar los cambios locales nuevamente (sin aplicar los cambios del archivo específico)
-git stash apply --index
->>>>>>> dev
